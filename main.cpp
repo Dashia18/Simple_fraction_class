2c2811a9f857e15791925805a238de4301ad129c
dbobkova <das-bobkova@yandex.ru>
2015-07-13 15:51:32 +0400

    Remount some parameters from main.

--- main.cpp
+++ main.cpp
@@ -7,9 +7,14 @@ int main(){
 	Simple_fraction sf_b;
 	bool fa, fb;
 
-	fa = sf_a.set_simple_fraction(3,0);
-	fb = sf_b.set_simple_fraction(1,2);
+	sf_a.set_simple_fraction(3,0);
+	sf_b.set_simple_fraction(1,2);
 
+	if(sf_a.set_simple_fraction(3,0) == false)
+	{
+		fa=0;
+		return -1;
+	}
 	sf_a.print_simple_fraction("sf_a");
 	sf_b.print_simple_fraction("sf_b");
 
